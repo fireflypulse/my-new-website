327347815937bb5cbb861efd2927b67a70cd928b
Michael Gleason <michaelgleason@Michaels-MacBook-Pro.local>
2014-04-21 16:40:02 -0700

    Started work on javascript.
    * Currently using jquery for on document ready
    * javascript function for fading in.

---
+++ javascripts/application.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  // $('#navigation').fadeIn(1000);
+
+  // fadeIn('#navigation')
+
+})
+
+function fadeIn(el) {
+  el.style.opacity = 0;
+
+  var last = +new Date();
+  var tick = function() {
+    el.style.opacity = +el.style.opacity + (new Date() - last) / 400;
+    last = +new Date();
+
+    if (+el.style.opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
+    }
+  };
+
+  tick();
+}
