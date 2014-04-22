9145f1057e4dcbe2198c764598fa366096c245ed
Michael Gleason <michaelgleason@Michaels-MacBook-Pro.local>
2014-04-21 18:56:27 -0700

    MVC javascript for fadein.
    To be done:
    * Adding event listeners for About and Contact pages
    * Adding views for About and Contact pages

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,30 +1,36 @@
 $(document).ready(function(){
 
-  // $('#navigation').fadeIn(1000);
+  var navigation = $('#navigation')[0];
 
   // fadeIn('#navigation')
-
-  var navbar = document.querySelectorAll('#navigation')[0];
-  fadeIn(navbar);
+  var myWebsite = new MyApp.Binder()
+  myWebsite.controller.fadeIn(navigation)
 })
 
+var MyApp = function() {}
 
-
-
-
-
-function fadeIn(el) {
-  el.style.opacity = 0;
-
-  var last = +new Date();
-  var tick = function() {
-    el.style.opacity = +el.style.opacity + (new Date() - last) / 1500;
-    last = +new Date();
-
-    if (+el.style.opacity < 1) {
-      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
-    }
-  };
-
-  tick();
+MyApp.Binder = function() {
+  this.controller = new MyApp.Controller();
 }
+
+MyApp.Controller = function() {}
+
+MyApp.Controller.prototype = {
+
+  fadeIn: function(el) {
+    el.style.opacity = 0;
+
+    var last = +new Date();
+    var tick = function() {
+      el.style.opacity = +el.style.opacity + (new Date() - last) / 1500;
+      last = +new Date();
+
+      if (+el.style.opacity < 1) {
+        (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
+      }
+    };
+
+    tick();
+  }
+
+};
